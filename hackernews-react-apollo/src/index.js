14f0763d1e62897399c0d627b982378b0e43cef2
Jason Prince <jasoncprince@gmail.com>
2019-10-31 16:36:28 -0600

    Authentication

--- hackernews-react-apollo/src/index.js
+++ hackernews-react-apollo/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
+import { AUTH_TOKEN } from "./constants";
 
 // 1
 import { ApolloProvider } from "react-apollo";
@@ -10,15 +11,26 @@ import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { BrowserRouter } from "react-router-dom";
+import { setContext } from "apollo-link-context";
 
 // 2
 const httpLink = createHttpLink({
   uri: "http://localhost:4000"
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ""
+    }
+  };
+});
+
 // 3
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
